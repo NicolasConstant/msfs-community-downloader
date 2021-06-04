3776940960cfb61e6292bbe8af5dc9f56bde195e
Nicolas Constant <github@nicolas-constant.com>
2021-06-04 19:00:30 -0400

    created UI for export

--- src/app/import/export-package/export-package.component.ts
+++ src/app/import/export-package/export-package.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-export-package',
-  templateUrl: './export-package.component.html',
-  styleUrls: ['./export-package.component.scss']
+    selector: 'app-export-package',
+    templateUrl: './export-package.component.html',
+    styleUrls: ['./export-package.component.scss']
 })
 export class ExportPackageComponent implements OnInit {
+    faTimes = faTimes;
+    
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    saveFile(): boolean {
+        return false;
+    }
 
 }
